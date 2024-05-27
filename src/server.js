e1c02781a9eca10c37fab45624b04a12a07a0ff1
kimdahee0815 <billionairedahee@gmail.com>
2024-05-27 18:09:23 +0900

    feat: created Router structure

--- src/server.js
+++ src/server.js
@@ -9,17 +9,29 @@ const logger = morgan('dev');
 app.use(logger);
 
 const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send('HOME');
+
+globalRouter.get('/', handleHome);
+
 const userRouter = express.Router();
+
+const handleEditUser = (req, res) => {
+    res.send('Edit user');
+};
+
+userRouter.get('/edit', handleEditUser);
+
 const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send('Watch Video!');
+
+videoRouter.get('/watch', handleWatchVideo);
 
 app.use('/', globalRouter);
 app.use('/videos', videoRouter);
 app.use('/users', userRouter);
 
-const handleHome = (req, res) => {
-    console.log(`Somebody is trying to go :${req.url}!`);
-    return res.send('<h1>i STILL LOVE YOU</h1>');
-};
 const handleLogin = (req, res) => {
     return res.send({ message: 'login here' });
 };

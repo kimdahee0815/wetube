66bd90e3b068d7c2c633e9ef81388be9fda7c9ae
kimdahee0815 <billionairedahee@gmail.com>
2024-05-27 18:19:00 +0900

    refactor: make routers separate file

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const app = express();
 
@@ -8,41 +11,9 @@ const PORT = 4000;
 const logger = morgan('dev');
 app.use(logger);
 
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send('HOME');
-
-globalRouter.get('/', handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => {
-    res.send('Edit user');
-};
-
-userRouter.get('/edit', handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send('Watch Video!');
-
-videoRouter.get('/watch', handleWatchVideo);
-
 app.use('/', globalRouter);
 app.use('/videos', videoRouter);
 app.use('/users', userRouter);
-
-const handleLogin = (req, res) => {
-    return res.send({ message: 'login here' });
-};
-
-// const handleProtected = (req, res, next) => {
-//     return res.send('welcome to the private lounge.');
-// };
-
-app.get('/', handleHome);
-
-app.get('/login', handleLogin);
 
 const handleListening = () => console.log(`Server Listening on port http://localhost:${PORT} 👌`);
 

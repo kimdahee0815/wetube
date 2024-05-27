5a70d6a9fe68262985991248930c83d90959f6ac
kimdahee0815 <billionairedahee@gmail.com>
2024-05-27 19:05:55 +0900

    refactor: created controller files

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { join } from '../controllers/userController';
+import { trending } from '../controllers/videoController';
 
 const globalRouter = express.Router();
 
-const handleHome = (req, res) => res.send('HOME');
-
-globalRouter.get('/', handleHome);
+globalRouter.get('/', trending);
+globalRouter.get('/join', join);
 
 export default globalRouter;

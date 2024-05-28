99666bbdef186f02abec8a8d82942a854ee684fb
kimdahee0815 <billionairedahee@gmail.com>
2024-05-28 18:20:40 +0900

    feat: page not found 404.pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,7 +12,12 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
-    return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+    console.log(video);
+
+    if (video) {
+        return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+    }
+    return res.render("404", { pageTitle: "Video Not Found" });
 };
 
 export const getEdit = (req, res) => {

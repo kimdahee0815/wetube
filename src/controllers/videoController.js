8cafdd030fa4341978df3e26bda14c9023691e7c
kimdahee0815 <billionairedahee@gmail.com>
2024-05-28 19:19:52 +0900

    feat: deleteVideo using findByIdAndDelete

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -67,3 +67,11 @@ export const postUpload = async (req, res) => {
         return res.render("upload", { pageTitle: "Upload Video", errorMessage: error._message });
     }
 };
+
+export const deleteVideo = async (req, res) => {
+    const { id } = req.params;
+    //delete the video
+    const removedVid = await Video.findByIdAndDelete(id);
+    console.log(removedVid);
+    return res.redirect("/");
+};

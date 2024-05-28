9d16b01520c0ef843480d092edc06897fd0ca4e6
kimdahee0815 <billionairedahee@gmail.com>
2024-05-28 15:18:00 +0900

    feat: Video.find promise async await to get videos data

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,44 +1,22 @@
-const videos = [
-    {
-        title: "First Video",
-        rating: 4,
-        comments: 2,
-        createdAt: "2 mnutes ago",
-        views: 59,
-        id: 1,
-    },
-    {
-        title: "Second Video",
-        rating: 4,
-        comments: 2,
-        createdAt: "2 mnutes ago",
-        views: 59,
-        id: 2,
-    },
-    {
-        title: "Third Video",
-        rating: 4,
-        comments: 2,
-        createdAt: "2 mnutes ago",
-        views: 59,
-        id: 3,
-    },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-    return res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+    try {
+        const videos = await Video.find({});
+        return res.render("home", { pageTitle: "Home", videos });
+    } catch (err) {
+        return res.render("server-error");
+    }
 };
 
 export const watch = (req, res) => {
     const { id } = req.params;
-    const video = videos[id - 1];
-    return res.render("watch", { pageTitle: `Watching: ${video.title}`, id, video });
+    return res.render("watch", { pageTitle: `Watching: ${video.title}` });
 };
 
 export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id - 1];
-    res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+    res.render("edit", { pageTitle: `Editing: ${video.title}` });
 };
 
 export const postEdit = (req, res) => {
@@ -46,8 +24,6 @@ export const postEdit = (req, res) => {
     console.log(req.body);
     const { id } = req.params;
     const { title } = req.body;
-    const video = videos[id - 1];
-    video.title = title;
     return res.redirect(`/videos/${id}`);
 };
 
@@ -59,14 +35,5 @@ export const postUpload = (req, res) => {
     //here we will add a video to videos array
     console.log(req.body);
     const { title } = req.body;
-    const newVideo = {
-        title,
-        rating: 0,
-        comments: 0,
-        createdAt: "just now",
-        views: 0,
-        id: videos.length + 1,
-    };
-    videos.push(newVideo);
     return res.redirect("/");
 };

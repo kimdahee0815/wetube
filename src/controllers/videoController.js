5e271346123d21a857dbb565f378e40d4d82d03f
kimdahee0815 <billionairedahee@gmail.com>
2024-05-28 09:35:27 +0900

    feat: pug watch file with current year

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,6 @@
 export const trending = (req, res) => res.render("home");
 
-export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
-};
+export const see = (req, res) => res.render("watch");
 
 export const edit = (req, res) => {
     console.log(req.params);

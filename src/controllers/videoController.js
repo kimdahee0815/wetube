bb7667b5e27b8bcb8547efdf7db1d52df31e1167
kimdahee0815 <billionairedahee@gmail.com>
2024-05-28 18:09:11 +0900

    feat: findById from mongoDB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,9 +9,10 @@ export const home = async (req, res) => {
     }
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
     const { id } = req.params;
-    return res.render("watch", { pageTitle: `Watching: ${video.title}` });
+    const video = await Video.findById(id);
+    return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
 
 export const getEdit = (req, res) => {

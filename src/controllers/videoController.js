d8bc4a7d4c6f8f8e432a4fca036c382019128e53
kimdahee0815 <billionairedahee@gmail.com>
2024-05-28 10:28:13 +0900

    feat: foreach inside pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,10 @@ const fakeUser = {
     loggedIn: false,
 };
 
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser });
+export const trending = (req, res) => {
+    const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    return res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
 
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 

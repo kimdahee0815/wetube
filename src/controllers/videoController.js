fc851e58d886ddf9134d8066566d68de06b06c73
kimdahee0815 <billionairedahee@gmail.com>
2024-05-28 18:57:32 +0900

    feat: middleware for save before schema

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -39,7 +39,7 @@ export const postEdit = async (req, res) => {
     await Video.findByIdAndUpdate(id, {
         title,
         description,
-        hashtags: hashtags.split(",").map((word) => (word.startsWith("#") ? word.trim() : `#${word.trim()}`)),
+        hashtags,
     });
     // video.title = title;
     // video.description = description;
@@ -59,7 +59,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            hashtags: hashtags.split(",").map((word) => (word.startsWith("#") ? word.trim() : `#${word.trim()}`)),
+            hashtags,
         });
         return res.redirect("/");
     } catch (error) {

71070c7a1066ea80e7dd67d358af8ccacaea0d94
kimdahee0815 <billionairedahee@gmail.com>
2024-05-29 10:30:30 +0900

    feat: form validation for email and username, password confirmation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,19 @@
 import User from "../models/User";
 
-export const getJoin = (req, res) => res.render("join", { pageTitle: "Create Account" });
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 
 export const postJoin = async (req, res) => {
-    console.log(req.body);
-    const { name, username, email, password, location } = req.body;
+    try {
+    } catch (error) {}
+
+    const { name, username, email, password, password2, location } = req.body;
+    const pageTitle = "Join";
+    if (password !== password2)
+        return res.render("join", { pageTitle, errorMessage: "Password confirmation does not match." });
+
+    const exists = await User.exists({ $or: [{ username }, { email }] });
+    if (exists) return res.render("join", { pageTitle, errorMessage: "This username/email is already taken." });
+
     await User.create({
         name,
         username,

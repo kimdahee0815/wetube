c66ad32d36452bf621742f72f0f77dccc3db5178
kimdahee0815 <billionairedahee@gmail.com>
2024-05-29 17:30:13 +0900

    feat: session with mongo-store, session data saved only when the user loggedIn

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -20,8 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
     session({
         secret: "Hello!",
-        resave: true,
-        saveUninitialized: true,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
     })
 );
 // app.use((req, res, next) => {
@@ -34,10 +36,10 @@ app.use(
 // });
 
 app.use(localsMiddleware);
-app.get("/add-one", (req, res, next) => {
-    req.session.potato += 1;
-    return res.send(`${req.session.id}\n${req.session.potato}`);
-});
+// app.get("/add-one", (req, res, next) => {
+//     req.session.potato += 1;
+//     return res.send(`${req.session.id}\n${req.session.potato}`);
+// });
 
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);

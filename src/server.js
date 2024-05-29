e2f29dd33fa3157f206ac2b764abc0df997b15d5
kimdahee0815 <billionairedahee@gmail.com>
2024-05-29 17:51:55 +0900

    feat: making string secret by using dotenv

--- src/server.js
+++ src/server.js
@@ -18,12 +18,16 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
     session({
-        secret: "Hello!",
+        secret: process.env.COOKIE_SECRET,
         resave: false,
         saveUninitialized: false,
-        store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+        // cookie: {
+        //     maxAge: 20000,
+        // },
+        store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
     })
 );
 // app.use((req, res, next) => {

c85cdf9df259f441b3c352d3d4a47c8dfb95f56f
kimdahee0815 <billionairedahee@gmail.com>
2024-06-03 11:16:17 +0900

    feat: change of video status bar for video currentTime change

--- src/frontend/js/videoPlayer.js
+++ src/frontend/js/videoPlayer.js
@@ -4,6 +4,7 @@ const muteBtn = document.getElementById("mute");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("volume");
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -55,11 +56,21 @@ const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(11
 
 const handleLoadedMetadata = () => {
     totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeupdate = () => {
     console.log(video.currentTime);
     currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (event) => {
+    const {
+        target: { value },
+    } = event;
+    console.log(value);
+    video.currentTime = value;
 };
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -67,3 +78,4 @@ muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeupdate);
+timeline.addEventListener("input", handleTimelineChange);

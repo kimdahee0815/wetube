45d0b9439c989461e11c5314f310de9a8bc01ffa
kimdahee0815 <billionairedahee@gmail.com>
2024-06-04 11:33:11 +0900

    feat: increase view of the video apiRouter

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import apiRouter from "./routers/apiRouter";
 import { localsMiddleware } from "./middlewares";
 
 console.log(process.cwd());
@@ -50,5 +51,6 @@ app.use("/assets", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
+app.use("/api", apiRouter);
 
 export default app;

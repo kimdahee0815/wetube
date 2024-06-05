5da5d113d951a19fb0b021181698a032f0fa67e7
kimdahee0815 <billionairedahee@gmail.com>
2024-06-05 09:14:31 +0900

    feat: ffmpeg changing webm to mp4, creating thumbnail and download

--- src/server.js
+++ src/server.js
@@ -31,6 +31,12 @@ app.use(
         store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
     })
 );
+
+app.use((req, res, next) => {
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    next();
+});
 // app.use((req, res, next) => {
 //     req.sessionStore.all((error, sessions) => {
 //         console.log(sessions);

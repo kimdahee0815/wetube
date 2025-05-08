348f62ce5e9614816f2caabfe566e8eadfceec51
kimdahee0815 <billionairedahee@gmail.com>
2025-05-08 15:45:19 +0900

    style: search baR

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,1 +1,10 @@
 import "../scss/styles.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const searchToggle = document.querySelector(".search-toggle");
+  const searchForm = document.querySelector(".search__form");
+
+  searchToggle.addEventListener("click", () => {
+    searchForm.classList.toggle("show");
+  });
+});

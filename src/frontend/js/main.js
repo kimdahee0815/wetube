86b1ef70669f58967379836a4384d893cee36102
kimdahee0815 <kimdahee0815@gmail.com>
2026-07-04 16:42:21 +0900

    style: search bar style

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -4,10 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const searchToggle = document.querySelector(".search-toggle");
     const searchForm = document.querySelector(".header__search");
 
-    searchToggle.addEventListener("click", () => {
-        searchForm.classList.toggle("show");
-        if (searchForm.classList.contains("show")) {
-            searchForm.querySelector("input").focus();
-        }
-    });
+    if (searchToggle && searchForm) {
+        searchToggle.addEventListener("click", () => {
+            searchForm.classList.toggle("show");
+            if (searchForm.classList.contains("show")) {
+                searchForm.querySelector("input").focus();
+            }
+        });
+    }
 });

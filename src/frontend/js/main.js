259b677fe9c3fe17bd90f9cdea5d013ab97681d5
kimdahee0815 <kimdahee0815@gmail.com>
2026-07-04 16:28:28 +0900

    style: style change for search bar

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,10 +1,13 @@
 import "../scss/styles.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const searchToggle = document.querySelector(".search-toggle");
-  const searchForm = document.querySelector(".search__form");
+    const searchToggle = document.querySelector(".search-toggle");
+    const searchForm = document.querySelector(".header__search");
 
-  searchToggle.addEventListener("click", () => {
-    searchForm.classList.toggle("show");
-  });
+    searchToggle.addEventListener("click", () => {
+        searchForm.classList.toggle("show");
+        if (searchForm.classList.contains("show")) {
+            searchForm.querySelector("input").focus();
+        }
+    });
 });
